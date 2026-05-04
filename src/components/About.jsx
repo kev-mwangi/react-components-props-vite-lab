b8b35243eb8d7462f966f514b6dbe15ebec496f2
Kelvin <kevinmwas007@gmail.com>
2026-05-04 13:00:48 +0300

    changed the logo image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 function About({ image, about }) {
-  const defaultImage = "https://images.unsplash.com/photo-1544907800-b98ac42cd27d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bG9nbyUyMG9mJTIwYmxvZ3xlbnwwfHwwfHx8MA%3D%3D";
+  const defaultImage = "https://via.placeholder.com/150";
 
   return (
     <aside>
